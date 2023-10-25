3b970b591d4b0b116933db15a4368876230a20cc
Marco Kuoni <marco@lemonbrain.ch>
2023-10-25 12:41:03 +0200

    Started with a new article

--- essays/9_porting_third_party_to_webassembly/README.de.md
+++ essays/9_porting_third_party_to_webassembly/README.de.md
@@ -1,4 +1,85 @@
 # Porting Third Party to WebAssembly
+Die Idee dieses Artikels ist, dass wir eine komplexere Applikation (wie sie in einer C++ Bibliothek bestehen könnte) nehmen und diese in WebAssembly portieren. Dabei werden wir diese zuerst in einem normalen C++ Programm testen und dann in WebAssembly. Wir werden dabei auf die Probleme eingehen, die wir dabei antreffen und wie wir diese lösen können.
+
+## Bitmap Applikation
+Die Applikation benutzt die `bitmap_image.hpp` von [Arash Partow](http://partow.net/programming/bitmap/index.html), welche dazu verwendet wird um verschiedene Bitmaps zu erzeugen.
+
+```cpp
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "bitmap_image.hpp"
+
+void cartesian()
+{
+    ...
+}
+
+void fractal()
+{
+    ...
+}
+
+int main(int argc, char **argv)
+{
+   int choosen = 0;
+   if(argc > 1)
+   {
+      choosen = std::atoi(argv[1]);
+   }
+
+   switch(choosen) {
+      case 1:
+         cartesian();
+         break;
+      case 2:
+         fractal();
+         break;
+      default:
+         printf("Usage: %s [1|2]\n", argv[0]);
+         break;
+   }
+   
+   return 0;
+}
+
+```
+
+Bevor dies nun aber gemacht werden kann muss das Programm zuerst kompiliert werden. Dazu ist bereits ein Makefile vorhanden.
+
+```Makefile
+COMPILER      = -c++
+OPTIONS       = -ansi -pedantic-errors -Wall -Wall -Werror -Wextra -o
+LINKER_OPT    = -L/usr/lib -lstdc++ -lm
+
+all: bitmap
+
+bitmap: bitmap.cpp bitmap_image.hpp
+	$(COMPILER) $(OPTIONS) bitmap bitmap.cpp $(LINKER_OPT)
+
+valgrind_check:
+	valgrind --leak-check=full --show-reachable=yes --track-origins=yes -v ./bitmap
+
+clean:
+	rm -f core *.o *.bak *stackdump *~
+```
+
+Welches über `make` ausgeführt werden kann. Nun wird eine Datei `bitmap` erstellt, welche ausgeführt werden kann.
+
+```bash
+./bitmap 1
+./bitmap 2
+```
+
+---
+emscripten nicht installiert, lese auch meinen anderen Artikel oder folge der Anweisung
+https://emscripten.org/docs/getting_started/downloads.html oder unter Ubuntu `sudo apt intall emscripten`
+
+----
+
 
 sudo apt install libsfml-dev
 
---
+++ essays/9_porting_third_party_to_webassembly/cpp/bitmap.cpp
@@ -0,0 +1,240 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "bitmap_image.hpp"
+
+void cartesian()
+{
+   {
+      cartesian_canvas canvas(1000, 1000);
+
+      if (!canvas)
+      {
+         printf("cartesian() - Error - Failed to instantiate cartesian canvas(1000x1000) [1]\n");
+         return;
+      }
+
+      canvas.rectangle(canvas.min_x(), canvas.min_y(), canvas.max_x(), canvas.max_y());
+
+      canvas.horiztonal_line_segment(canvas.min_x(), canvas.max_x(), -400.0);
+
+      canvas.line_segment(-500.0, 600.0, 600.0, -500.0);
+
+      canvas.pen_width(3);
+
+      for (std::size_t i = 0; i < 160; i++)
+      {
+         std::size_t c_idx = i % (sizeof(palette_colormap) / sizeof(rgb_t));
+
+         canvas.pen_color(palette_colormap[c_idx].red, palette_colormap[c_idx].green, palette_colormap[c_idx].blue);
+
+         canvas.circle(0.0, 0.0, 3.0 * i);
+      }
+
+      canvas.image().save_image("cartesian_canvas01.bmp");
+   }
+
+   {
+      static const double pi = 3.14159265358979323846264338327950288419716939937510;
+
+      cartesian_canvas canvas(1000, 1000);
+
+      if (!canvas)
+      {
+         printf("cartesian() - Error - Failed to instantiate cartesian canvas(1000x1000) [2]\n");
+         return;
+      }
+
+      canvas.image().set_all_channels(0xFF);
+
+      canvas.pen_width(2);
+
+      unsigned int i = 0;
+
+      for (double x = -500; x < 500; x += 3, ++i)
+      {
+         std::size_t c_idx = i % (sizeof(palette_colormap) / sizeof(rgb_t));
+
+         canvas.pen_color(palette_colormap[c_idx].red, palette_colormap[c_idx].green, palette_colormap[c_idx].blue);
+
+         double radius = std::max(10.0,std::abs(80.0 * std::sin((1.0 / 80.0) * pi * x)));
+
+         double y = 400.0 * std::sin((1.0 / 200.0) * pi * x);
+
+         canvas.circle(x, y, radius);
+      }
+
+      canvas.image().save_image("cartesian_canvas02.bmp");
+   }
+}
+
+void fractal()
+{
+   const rgb_t* colormap[4] = {
+                                   hsv_colormap,
+                                   jet_colormap,
+                                 prism_colormap,
+                                   vga_colormap
+                              };
+
+   const unsigned int fractal_width  = 1200;
+   const unsigned int fractal_height =  800;
+
+   {
+      bitmap_image fractal_hsv  (fractal_width,fractal_height);
+      bitmap_image fractal_jet  (fractal_width,fractal_height);
+      bitmap_image fractal_prism(fractal_width,fractal_height);
+      bitmap_image fractal_vga  (fractal_width,fractal_height);
+
+      fractal_hsv  .clear();
+      fractal_jet  .clear();
+      fractal_prism.clear();
+      fractal_vga  .clear();
+
+      double    cr,    ci;
+      double nextr, nexti;
+      double prevr, previ;
+
+      const unsigned int max_iterations = 1000;
+
+      for (unsigned int y = 0; y < fractal_height; ++y)
+      {
+         for (unsigned int x = 0; x < fractal_width; ++x)
+         {
+            cr = 1.5 * (2.0 * x / fractal_width  - 1.0) - 0.5;
+            ci =       (2.0 * y / fractal_height - 1.0);
+
+            nextr = nexti = 0;
+            prevr = previ = 0;
+
+            for (unsigned int i = 0; i < max_iterations; i++)
+            {
+               prevr = nextr;
+               previ = nexti;
+
+               nextr =     prevr * prevr - previ * previ + cr;
+               nexti = 2 * prevr * previ + ci;
+
+               if (((nextr * nextr) + (nexti * nexti)) > 4)
+               {
+                  if (max_iterations != i)
+                  {
+                     double z = sqrt(nextr * nextr + nexti * nexti);
+
+                     #define log2(x) (std::log(1.0 * x) / std::log(2.0))
+
+                     unsigned int index = static_cast<unsigned int>
+                        (1000.0 * log2(1.75 + i - log2(log2(z))) / log2(max_iterations));
+                     #undef log2
+
+                     rgb_t c0 = colormap[0][index];
+                     rgb_t c1 = colormap[1][index];
+                     rgb_t c2 = colormap[2][index];
+                     rgb_t c3 = colormap[3][index];
+
+                     fractal_hsv  .set_pixel(x, y, c0.red, c0.green, c0.blue);
+                     fractal_jet  .set_pixel(x, y, c1.red, c1.green, c1.blue);
+                     fractal_prism.set_pixel(x, y, c2.red, c2.green, c2.blue);
+                     fractal_vga  .set_pixel(x, y, c3.red, c3.green, c3.blue);
+                  }
+
+                  break;
+               }
+            }
+         }
+      }
+
+      fractal_hsv  .save_image("mandelbrot_set_hsv.bmp"  );
+      fractal_jet  .save_image("mandelbrot_set_jet.bmp"  );
+      fractal_prism.save_image("mandelbrot_set_prism.bmp");
+      fractal_vga  .save_image("mandelbrot_set_vga.bmp"  );
+   }
+
+   {
+      bitmap_image fractal_hsv  (fractal_width,fractal_height);
+      bitmap_image fractal_jet  (fractal_width,fractal_height);
+      bitmap_image fractal_prism(fractal_width,fractal_height);
+      bitmap_image fractal_vga  (fractal_width,fractal_height);
+
+      fractal_hsv  .clear();
+      fractal_jet  .clear();
+      fractal_prism.clear();
+      fractal_vga  .clear();
+
+      const unsigned int max_iterations = 300;
+
+      const double cr = -0.70000;
+      const double ci =  0.27015;
+
+      double prevr, previ;
+
+      for (unsigned int y = 0; y < fractal_height; ++y)
+      {
+         for (unsigned int x = 0; x < fractal_width; ++x)
+         {
+            double nextr = 1.5 * (2.0 * x / fractal_width  - 1.0);
+            double nexti =       (2.0 * y / fractal_height - 1.0);
+
+            for (unsigned int i = 0; i < max_iterations; i++)
+            {
+               prevr = nextr;
+               previ = nexti;
+
+               nextr =     prevr * prevr - previ * previ + cr;
+               nexti = 2 * prevr * previ + ci;
+
+               if (((nextr * nextr) + (nexti * nexti)) > 4)
+               {
+                  if (max_iterations != i)
+                  {
+                     unsigned int index = static_cast<int>((1000.0 * i) / max_iterations);
+
+                     rgb_t c0 = colormap[0][index];
+                     rgb_t c1 = colormap[1][index];
+                     rgb_t c2 = colormap[2][index];
+                     rgb_t c3 = colormap[3][index];
+
+                     fractal_hsv  .set_pixel(x, y, c0.red, c0.green, c0.blue);
+                     fractal_jet  .set_pixel(x, y, c1.red, c1.green, c1.blue);
+                     fractal_prism.set_pixel(x, y, c2.red, c2.green, c2.blue);
+                     fractal_vga  .set_pixel(x, y, c3.red, c3.green, c3.blue);
+                  }
+
+                  break;
+               }
+            }
+         }
+      }
+
+      fractal_hsv  .save_image("julia_set_hsv.bmp"  );
+      fractal_jet  .save_image("julia_set_jet.bmp"  );
+      fractal_prism.save_image("julia_set_prism.bmp");
+      fractal_vga  .save_image("julia_set_vga.bmp"  );
+   }
+}
+
+int main(int argc, char **argv)
+{
+   int choosen = 0;
+   if(argc > 1)
+   {
+      choosen = std::atoi(argv[1]);
+   }
+
+   switch(choosen) {
+      case 1:
+         cartesian();
+         break;
+      case 2:
+         fractal();
+         break;
+      default:
+         printf("Usage: %s [1|2]\n", argv[0]);
+         break;
+   }
+   
+   return 0;
+}
